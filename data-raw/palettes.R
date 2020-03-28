286a07cf4bcca49ae2aef3f5f4d29b5ba81aa0c1
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-03-28 18:50:19 +0100

    fix new system

--- data-raw/palettes.R
+++ data-raw/palettes.R
@@ -11,3 +11,7 @@ brain_pals = list(
 )
 usethis::use_data(brain_pals, internal = TRUE, overwrite = TRUE)
 
+devtools::load_all("../../ggsegExtra/")
+jhu_3d <- ggsegExtra:::restruct_old_3datlas(jhu_3d)
+jhu_3d <- as_ggseg3d_atlas(jhu_3d)
+usethis::use_data(jhu_3d, internal = FALSE, overwrite = TRUE)
